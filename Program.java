a7178af106c8fd7d4fee7c6e00692a5a51109e9e
veenab10 <veenab.xworkz@gmail.com>
2023-12-07 11:34:30 +0530

    Example on method

---
+++ Program.java
@@ -0,0 +1,36 @@
+public class Program{
+    public static void main(String args[])
+    {
+        System.out.println("Start writing a code");
+        code();
+    }
+    public static void code()
+    {
+        System.out.println("program code written");
+        save();
+    }
+    public static void save()
+    {
+        System.out.println("Save the code with filename and extension with properly");
+        run();
+    }
+    public static void run()
+    {
+        System.out.println("Run the code");
+        debugg();
+    }
+    public static void debugg()
+    {
+        System.out.println("debugg the code");
+        compiler();
+    }
+    public static void compiler()
+    {
+        System.out.println("compile the code");
+        interpreter();
+    }
+    public static void interpreter()
+    {
+        System.out.println("After compilation interpreter is done");
+    }
+}
