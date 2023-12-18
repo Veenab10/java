885d0005818b7d1665b3eba07a4555cfdef9ddd7
veenab10 <veenab.xworkz@gmail.com>
2023-12-18 11:06:35 +0530

    Typecasting

---
+++ Digit.java
@@ -0,0 +1,49 @@
+public class Digit{
+	
+	public static void main(String args[])
+	{
+		
+		byte num=20;
+		short value=num;
+		System.out.println(value);
+	
+		short num1=400;
+		int value1=num1;
+		System.out.println(value1);
+
+		int num2=50000;
+		long value2=num2;
+		System.out.println(value2);
+
+		long num3=1111111111l;
+		int value3=(int)num3;
+		System.out.println(value3);
+
+		int num4=23279;
+		short value4=(short)num4;
+		System.out.println(value4);
+
+		short num5=89;
+		byte value5=(byte)num5;
+		System.out.println(value5);
+
+		int num6=97;
+		char value6=(char)num6;
+		System.out.println(value6);
+
+		char char1='v';
+		int value7=char1;
+		System.out.println(value7);
+
+		float num8=23.832f;
+		double value8=num8;
+		System.out.println(value8);
+		
+
+		double num9=89.32145d;
+		float value9=(float)num9;
+		System.out.println(value9);
+	
+
+	}
+}
