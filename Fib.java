6b1e7b1320ba899ebc5f7ea2547314fde707dfd8
veenab10 <veenab.xworkz@gmail.com>
2023-12-26 10:47:13 +0530

    Enum

--- Fib.java
+++ Fib.java
@@ -1,17 +1,17 @@
-class Fibonacci{
-	public static void main(String args[])
-	{
-		int f1=0,f2=1,f3,n;
-		n=Integer.parseInt(args[0]);
-		System.out.println("The fibonacci series upto"+n+"terms are:");
-		System.out.println(f1+"\n"+f2+"\n");
-		for (int i=2;i<n;i++)
-		{
-			f3=f1+f2;
-			f1=f2;
-			f2=f3;
-			System.out.println(f3+"\n");
-		}
-	}
+class Fibonacci
+{
+public static void main(String args[])
+{
+int f1=0,f2=1,f3,n;
+n=Integer.parseInt(args[0]);
+System.out.println("The Fibonacci series upto "+n+ " terms are : ");
+System.out.println(f1+"\n"+f2+"\n");
+for(int i=2;i<n;i++)
+{
+f3 = f1 + f2;
+f1 = f2;
+f2 = f3;
+System.out.println(f3+"\n");
 }
-		
+}
+}
