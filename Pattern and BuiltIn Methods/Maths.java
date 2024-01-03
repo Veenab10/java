bfbba647685332a5c69436fd190d01dec7f5d7c2
veenab10 <veenab.xworkz@gmail.com>
2024-01-03 10:41:56 +0530

    Pattern and Built In Methods

---
+++ Pattern and BuiltIn Methods/Maths.java
@@ -0,0 +1,21 @@
+public class Maths{
+
+	public static void main(String args[])
+	{
+	 	double value1=Math.cbrt(8);
+	 	System.out.println(value1);
+
+	 	double value2=Math.multiplyExact(2,3);
+	 	System.out.println(value2);
+	 
+		double value3=Math.exp(2);
+		System.out.println(value3);
+
+		int value4=Math.addExact(8,2);
+		System.out.println(value4);
+	
+		int value5=Math.subtractExact(10,5);
+		System.out.println(value5);
+
+	}
+}
---
+++ Pattern and BuiltIn Methods/Pattern1.java
@@ -0,0 +1,16 @@
+public class Pattern1{
+
+public static void main(String args[])
+{
+	int n=6;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i;j<=n;j++)
+		{
+			System.out.print("v\t");
+		}
+		System.out.print("\n");
+	}
+}
+}
+	
