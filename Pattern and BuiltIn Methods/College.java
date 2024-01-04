bb7c58008f37f3dbdbb946de92c4cf57ca8a9483
veenab10 <veenab.xworkz@gmail.com>
2024-01-04 09:46:21 +0530

    Pattern and String

---
+++ Pattern and BuiltIn Methods/College.java
@@ -0,0 +1,111 @@
+public class College{
+
+	public static void main(String args[])
+	{
+		String departmentName="CS";
+		System.out.println(departmentName);
+		System.out.println(departmentName==departmentName);
+		System.out.println("-------------------------------");
+
+		String fatherOfC="Dennis Ritchie";
+		System.out.println(fatherOfC);
+		System.out.println("-------------------------------");
+
+		String textBookNameOfC="BalaguruSwamy";
+		System.out.println(textBookNameOfC);
+		System.out.println("-------------------------------");
+
+		String backEndLanguage="SQL";
+		System.out.println(backEndLanguage);
+		backEndLanguage="Python";
+		System.out.println(backEndLanguage);
+		System.out.println("-------------------------------");
+
+		String frontEndLanguage="HTML";
+		System.out.println(frontEndLanguage);
+		frontEndLanguage="CSS";
+		System.out.println(frontEndLanguage);
+		System.out.println("-------------------------------");
+
+		String MiddleWareLanguage="Java Script";
+		System.out.println(MiddleWareLanguage);
+		System.out.println("-------------------------------");
+
+		String frameWorkName="SpringRoot";
+		System.out.println(frameWorkName);
+		System.out.println("-------------------------------");
+
+		String producerProgrammingLanguage="C Language";
+		System.out.println(producerProgrammingLanguage);
+		System.out.println("-------------------------------");
+
+		String queryLanguage="DBMS";
+		System.out.println(queryLanguage);
+		System.out.println("-------------------------------");
+
+		String collegeName="JSS";
+		System.out.println(collegeName);
+		collegeName="SMI";
+		System.out.println(collegeName);		
+		System.out.println("-------------------------------");
+		
+		String value1=new String("Banglore");
+		System.out.println(value1);
+		value1="Baglkot";
+		System.out.println(value1);
+		System.out.println("-------------------------------");
+
+		String value2=new String("Kannada");
+		System.out.println(value2);
+		value2="English";
+		System.out.println(value2);
+		System.out.println("-------------------------------");
+		
+		String value3=new String("NewYork");
+		System.out.println(value3);
+		System.out.println("-------------------------------");
+		
+		
+		String value4 =new String("SQL");
+		System.out.println(value4);
+		System.out.println(value4==backEndLanguage);
+		System.out.println("-------------------------------");
+
+		String value5=new String("CSS");
+		System.out.println(value5);
+		System.out.println(value5==frontEndLanguage);
+		System.out.println("-------------------------------");
+
+		String value6=new String("Linux");
+		System.out.println(value6);
+		System.out.println("-------------------------------");
+
+		String value7=new String("Python");
+		System.out.println(value7);
+		System.out.println("-------------------------------");
+	
+		String value8=new String("Mico Layout");
+		System.out.println(value8);
+		System.out.println("-------------------------------");
+
+		String value9=new String("Infoys");
+		System.out.println(value9);
+		System.out.println(value9==value9);
+		System.out.println("-------------------------------");
+
+		String value10=new String("Mysore");
+		System.out.println(value10);
+		System.out.println(value10==value10);
+		System.out.println("-------------------------------");
+	}
+}
+
+
+
+
+
+
+
+		
+		
+		
